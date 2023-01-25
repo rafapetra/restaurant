1aa0f9f5510f8c381b1f3e7aab05aa590cf1da84
Rafael Petrachini <raffp@me.com>
2023-01-24 21:50:30 -0800

    first commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading, subHeading } from "../../components";
+
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Quote</p>
+       </div>
+        <p className="p__opensans">Qupte 2</p>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Chef;
---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Mediterranean Cafe" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        We are happy to announce that we’ve fully reopened for lunch and dinner!
+        Both dine-in and patio are available. Our new hours are 11:00 am - 11:00
+        pm every day. We look forward to seeing all of you again!
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
+  </div>
+);
+
+export default Header;
