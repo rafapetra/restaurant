a4b78661979b8aa4a1eab96aaccea8e87b1ea9c1
Rafael Petrachini <raffp@me.com>
2023-02-16 22:06:32 -0800

    add jsx pages for Chef, AboutUs, Menu and Gallery

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -17,9 +17,9 @@ const Chef = () => (
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote" />
-          <p className="p__opensans">Quote</p>
+          <p className="p__opensans">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
        </div>
-        <p className="p__opensans">Qupte 2</p>
+        <p className="p__opensans">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
       </div>
 
     </div>
